8996950170f17770e1e4b5bda2ff8d7247b8cdaf
Jeff Nyman <jeffnyman@gmail.com>
2022-10-16 10:54:47 -0500

    Initial project commit.

---
+++ script.js
@@ -0,0 +1,7 @@
+window.addEventListener("load", () => {
+  const viewport = document.getElementById("viewport");
+  const context = viewport.getContext("2d");
+
+  viewport.width = 500;
+  viewport.height = 500;
+});
