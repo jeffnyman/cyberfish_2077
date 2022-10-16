d76286263b9f8c5b5a8ee0b82a52081a4b49a3ad
Jeff Nyman <jeffnyman@gmail.com>
2022-10-16 12:03:53 -0500

    Addition of Player module.

--- src/game.js
+++ src/game.js
@@ -1,7 +1,15 @@
+import { Player } from "./player.js";
+
 class Game {
   constructor(width, height) {
+    // Dimensions
+
     this.width = width;
     this.height = height;
+
+    // State
+
+    this.player = new Player(this);
   }
 }
 
---
+++ src/player.js
@@ -0,0 +1,17 @@
+class Player {
+  constructor(game) {
+    this.game = game;
+
+    // Dimensions
+
+    this.width = 120;
+    this.height = 190;
+
+    // Location
+
+    this.x = 20;
+    this.y = 100;
+  }
+}
+
+export { Player };
