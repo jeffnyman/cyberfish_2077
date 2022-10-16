34e572e04f02f12c5dc43887fb6fd7f218e548b5
Jeff Nyman <jeffnyman@gmail.com>
2022-10-16 11:12:12 -0500

    Addition of Game module.

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
+import { Game } from "./src/game.js";
+
 window.addEventListener("load", () => {
   const viewport = document.getElementById("viewport");
   const context = viewport.getContext("2d");
 
   viewport.width = 500;
   viewport.height = 500;
+
+  const game = new Game(viewport.width, viewport.height);
 });
---
+++ src/game.js
@@ -0,0 +1,8 @@
+class Game {
+  constructor(width, height) {
+    this.width = width;
+    this.height = height;
+  }
+}
+
+export { Game };
