397a31bfb9a4231248a430c4394629c55012aab4
Jeff Nyman <jeffnyman@gmail.com>
2022-10-17 09:06:00 -0500

    Add win and loss messages.

--- src/ui.js
+++ src/ui.js
@@ -29,6 +29,38 @@ class UserInterface {
       context.fillRect(20 + 10 * i, 50, 3, 20);
     }
 
+    // Display game over messages
+
+    if (this.game.gameOver) {
+      context.textAlign = "center";
+      context.fillStyle = "white";
+
+      let message1;
+      let message2;
+
+      if (this.game.bounty > this.game.winningBounty) {
+        message1 = "Bounty Collected!";
+        message2 = "Well done on the hunt!";
+      } else {
+        message1 = "Bounty Not Collected!";
+        message2 = "Better luck on the next hunt!";
+      }
+
+      context.font = "50px " + this.fontFamily;
+      context.fillText(
+        message1,
+        this.game.width * 0.5,
+        this.game.height * 0.5 - 40,
+      );
+
+      context.font = "25px " + this.fontFamily;
+      context.fillText(
+        message2,
+        this.game.width * 0.5,
+        this.game.height * 0.5 + 40,
+      );
+    }
+
     context.restore();
   }
 }
