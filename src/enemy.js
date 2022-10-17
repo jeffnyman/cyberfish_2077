131899d579056441988e72e0ee0ac0281c885887
Jeff Nyman <jeffnyman@gmail.com>
2022-10-17 06:46:36 -0500

    Addition of Enemy module; enemies generated.

---
+++ src/enemy.js
@@ -0,0 +1,48 @@
+class Enemy {
+  constructor(game) {
+    this.game = game;
+
+    // Location
+
+    this.x = this.game.width;
+
+    // Movement
+
+    this.speedX = Math.random() * -1.5 - 0.5;
+
+    // Conditions
+
+    this.outOfPlay = false;
+  }
+
+  update() {
+    this.x += this.speedX;
+
+    // Check if the enemy has moved off the screen.
+    if (this.x + this.width < 0) {
+      this.outOfPlay = true;
+    }
+  }
+
+  draw(context) {
+    context.fillStyle = "red";
+    context.fillRect(this.x, this.y, this.width, this.height);
+  }
+}
+
+class Angler1 extends Enemy {
+  constructor(game) {
+    super(game);
+
+    // Dimensions
+
+    this.width = 228 * 0.2;
+    this.height = 169 * 0.2;
+
+    // Location
+
+    this.y = Math.random() * (this.game.height * 0.9 - this.height);
+  }
+}
+
+export { Angler1 };
--- src/game.js
+++ src/game.js
@@ -1,3 +1,4 @@
+import { Angler1 } from "./enemy.js";
 import { InputHandler } from "./input.js";
 import { Player } from "./player.js";
 import { UserInterface } from "./ui.js";
@@ -19,15 +20,48 @@ class Game {
 
     // Keeps track of all actions (key press events) by the player.
     this.actions = [];
+
+    // Keeps track of all active enemies on the level.
+    this.enemies = [];
+
+    // Mechanics
+
+    this.enemyTimer = 0;
+    this.enemyInterval = 1000;
   }
 
   update(deltaTime) {
     this.player.update(deltaTime);
+
+    // Handle enemy movement.
+
+    this.enemies.forEach((enemy) => {
+      enemy.update();
+    });
+
+    this.enemies = this.enemies.filter((enemy) => !enemy.outOfPlay);
+
+    if (this.enemyTimer > this.enemyInterval) {
+      this.addEnemy();
+      this.enemyTimer = 0;
+    } else {
+      this.enemyTimer += deltaTime;
+    }
   }
 
   draw(context) {
     this.player.draw(context);
     this.ui.draw(context);
+
+    // Draw enemies.
+
+    this.enemies.forEach((enemy) => {
+      enemy.draw(context);
+    });
+  }
+
+  addEnemy() {
+    this.enemies.push(new Angler1(this));
   }
 }
 
